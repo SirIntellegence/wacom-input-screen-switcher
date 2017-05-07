b500d255352c23442e8255a1fb7d26a6a5423c3f
sirintellegence <SirIntellegence@users.noreply.github.com>
2017-05-06 20:38:30 -0600

    Now working on stupid transform stuff....

--- wiss.py
+++ wiss.py
@@ -25,9 +25,9 @@ import re
 # from subprocess import call
 
 # transform matrix values
-#  | scale x    0           xtranslate|
-#  | 0          yscale      ytranslate|
-#  | 0          0           1         |
+#  | x scale    0           x translate|
+#  | 0          y scale     y translate|
+#  | 0          0           1          |
 
 
 def eprint(*args, **kwargs):
@@ -46,6 +46,61 @@ class device:
         pass
 
 
+class transform:
+    xScale = 1
+    yScale = 1
+    xTranslate = 0
+    yTranslate = 0
+
+    # URL that generated this regex:
+    # http://txt2re.com/index-python.php3?s=0.428571,%200.000000,%200.000000,%200.000000,%201.000000,%200.000000,%200.000000,%200.000000,%201.000000&14&-133&54&8&-134&55&9&16&61&-140&13&60&-139&12&59&-138&11&58&-137&15&57&-136&10&56&-135
+    _re1 = '([+-]?\\d*\\.\\d+)(?![-+0-9\\.])'  # Float 1
+    _re2 = '(,)'  # Any Single Character 1
+    _re3 = '(\\s+)'  # White Space 1
+    _re4 = '([+-]?\\d*\\.\\d+)(?![-+0-9\\.])'  # Float 2
+    _re5 = '(,)'  # Any Single Character 2
+    _re6 = '(\\s+)'  # White Space 2
+    _re7 = '([+-]?\\d*\\.\\d+)(?![-+0-9\\.])'  # Float 3
+    _re8 = '(,)'  # Any Single Character 3
+    _re9 = '(\\s+)'  # White Space 3
+    _re10 = '([+-]?\\d*\\.\\d+)(?![-+0-9\\.])'  # Float 4
+    _re11 = '(,)'  # Any Single Character 4
+    _re12 = '(\\s+)'  # White Space 4
+    _re13 = '([+-]?\\d*\\.\\d+)(?![-+0-9\\.])'  # Float 5
+    _re14 = '(,)'  # Any Single Character 5
+    _re15 = '(\\s+)'  # White Space 5
+    _re16 = '([+-]?\\d*\\.\\d+)(?![-+0-9\\.])'  # Float 6
+    _re17 = '(,)'  # Any Single Character 6
+    _re18 = '(\\s+)'  # White Space 6
+    _re19 = '([+-]?\\d*\\.\\d+)(?![-+0-9\\.])'  # Float 7
+    _re20 = '(,)'  # Any Single Character 7
+    _re21 = '(\\s+)'  # White Space 7
+    _re22 = '([+-]?\\d*\\.\\d+)(?![-+0-9\\.])'  # Float 8
+    _re23 = '(,)'  # Any Single Character 8
+    _re24 = '(\\s+)'  # White Space 8
+    _re25 = '([+-]?\\d*\\.\\d+)(?![-+0-9\\.])'  # Float 9
+
+    transformRegEx = re.compile(_re1 + _re2 + _re3 + _re4 + _re5 + _re6 + _re7 +
+                                _re8 + _re9 + _re10 + _re11 + _re12 + _re13 +
+                                _re14 + _re15 + _re16 + _re17 + _re18 + _re19 +
+                                _re20 + _re21 + _re22 + _re23 + _re24 + _re25,
+                                re.IGNORECASE | re.DOTALL)
+
+    def parse(input: str):
+        m = transformRegEx.search(input)
+        xScale = int(m.group(1))
+        xTranslate = int(m.group(7))
+        yScale = int(m.group(13))
+        yTranslate = int(m.group(16))
+        pass
+
+    # def __init__(self, transformMatch):
+
+
+class screen:
+    transform = transform()
+
+
 class wiss:
     def __init__(self):
 
@@ -60,11 +115,14 @@ class wiss:
         except getopt.GetoptError:
             self.__usage()
             sys.exit(2)
+        if (len(opts) > 0 or len(args) > 0):
+            eprint("Program arguments are not supported at this time. Sorry.")
+            sys.exit(3)
         devices = self.getDevices()
         if (len(devices) < 1):
             eprint("No tablet devices were found")
-            sys.exit(3)
-
+            sys.exit(4)
+        screens = self.getScreens()
         pass
 
     def getDevices(self) -> list:
@@ -76,29 +134,36 @@ class wiss:
             results.append(device(m[0], int(m[1]), m[2].strip()))
         return results
 
-    def _getCurrOutput(self, id: int) -> str:
+    def _getCurrOutput(self, device: device) -> str:
+        pass
 
-    def _getOutputList(self) -> list:
-        pattern = re.compile(r"\S+")
-        # note: may return invalid results if there is more than one xscreen
-        tempResult = subprocess.check_output(["xrandr"], bufsize=100).
-            decode("utf-8")
-        first = false
+    def getScreens(self) -> list:
+        monitors = screeninfo.get_monitors()
+        totalWidth = 0
+        totalHeight = 0
         results = []
-        for line in tempResult.split('\n'):
-            if first:
-                first = False
-                continue
-            if line[0] == ' '
-                continue
-            results.append(pattern.search(line).group(0))
-
         return results
 
+    # def _getOutputList(self) -> list:
+    #     pattern = re.compile(r"\S+")
+    #     # note: may return invalid results if there is more than one xscreen
+    #     tempResult = subprocess.check_output(
+    #         ["xrandr"], bufsize=100).decode("utf-8")
+    #     first = false
+    #     results = []
+    #     for line in tempResult.split('\n'):
+    #         if first:
+    #             first = False
+    #             continue
+    #         if line[0] == ' '
+    #             continue
+    #         results.append(pattern.search(line).group(0))
+
+    #     return results
+
     def getMappedArea(self, device: device):
-        # device.
         call()
-        results = []
+        result = transform()
         pass
 
 
